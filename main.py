0e91dec21f3a32685b85bab44d416cc6e8813885
unknown <saadsyed950@gmail.com>
2025-01-20 15:54:18 +0530

    Chatbot function defined

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 genai.configure(api_key=API_KEY)
 
+def chatWithGemini(prompt):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(prompt)
+    return response.text
